6c9a0c9e470c078ae960fe43957f1072f204f8b8
Angie Quin <aquinones100@gmail.com>
2018-11-15 16:48:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,44 +1,37 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles']
 
-function addElementToBeginningOfArray (chocolateBars, candy) {
-  var chocolateBars2 = ["candy", ...chocolateBars];
-  return chocolateBars2;
+function addElementToBeginningOfArray(array, element){
+  var array = [1];
+  var array = ["foo", ...array]
+  return array
 }
 
-function destructivelyAddElementToBeginningOfArray (chocolateBars, candy) {
-  chocolateBars.unshift ("candy");
-  return chocolateBars;
+addElementToBeginningOfArray(array, element);
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = [1];
+  array.unshift('foo')
+  return array
 }
 
-function addElementToEndOfArray (chocolateBars, candy) {
-  var chocolateBars3 = [...chocolateBars, "candy"];
-  return chocolateBars3;
+destructivelyAddElementToBeginningOfArray(array, element);
+
+function addElementToEndOfArray(array, element) {
+  var array = [1];
+  var array = [...array, "foo"]
+  return array
 }
 
-funtion destructivelyAddElementToEndOfArray (chocolateBars, candy) {
-  chocolateBars.push("candy");
-  return chocolateBars;
+addElementToEndOfArray(array, element);
+
+function destructivelyAddElementToEndOfArray(array, element){
+  var array = [1];
+  array.push('foo')
+  return array
 }
 
-function accessElementInArray (chocolateBars, 3) {
-  return chocolateBars[3];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
-  chocolateBars.shift(2);
-  return chocolateBars;
-}
-
-function removeElementFromBeginningOfArray (chocolateBars) {
-  return chocolateBars.slice(1);
-}
-
-function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
-  chocolateBars.pop();
-  return chocolateBars;
-}
-
-function removeElementFromEndOfArray (chocolateBars) {
-  var chocolateBars4 = chocolateBars.slice(0, chocolateBars.length - 3)
-  return chocolateBars4
-}
+destructivelyAddElementToEndOfArray(array, element);
