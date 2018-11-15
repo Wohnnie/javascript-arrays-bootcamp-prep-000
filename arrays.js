0ee7fa71fab2bcb437df38412ead09a376c46f13
Angie Quin <aquinones100@gmail.com>
2018-11-15 16:51:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,36 +2,40 @@ var chocolateBars = [
   'snickers',
   'hundred grand',
   'kitkat',
-  'skittles']
+  'skittles'];
 
 function addElementToBeginningOfArray(array, element){
   var array = [1];
-  var array = ['foo', ...array]
-  return array
+  var array = ['foo', ...array];
+  return array;
 }
 
 addElementToBeginningOfArray(array, element);
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var array = [1];
-  array.unshift('foo')
-  return array
+  array.unshift('foo');
+  return array;
 }
 
 destructivelyAddElementToBeginningOfArray(array, element);
 
 function addElementToEndOfArray(array, element) {
   var array = [1];
-  var array = [...array, 'foo']
-  return array
+  var array = [...array, 'foo'];
+  return array;
 }
 
 addElementToEndOfArray(array, element);
 
 function destructivelyAddElementToEndOfArray(array, element){
   var array = [1];
-  array.push('foo')
-  return array
+  array.push('foo');
+  return array;
 }
 
 destructivelyAddElementToEndOfArray(array, element);
+
+function accessElementInArray(array, element){
+
+}
