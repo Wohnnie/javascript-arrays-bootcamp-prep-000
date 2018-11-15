93559bdeb932dfc0049a1ce68ede7cebe68e229a
Angie Quin <aquinones100@gmail.com>
2018-11-15 17:13:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -57,3 +57,7 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop
 }
+
+function removeElementFromEndOfArray(array) {
+  return array.slice (0, array.length - 1) 
+}
