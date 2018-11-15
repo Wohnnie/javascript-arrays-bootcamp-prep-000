bcb6d186e9ad5842816c3a3cc89b85f168f81b59
Angie Quin <aquinones100@gmail.com>
2018-11-15 17:19:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,6 @@ function addElementToBeginningOfArray(array, element){
 addElementToBeginningOfArray(array, element);
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1];
   array.unshift("foo");
   return array
 }
